340e6668385608c650310678485fbb6a7efa894c
Ihor Yanda <62036865+lyo-n@users.noreply.github.com>
2022-12-05 10:00:55 +0200

    ut

--- client/pages/_app.jsx
+++ client/pages/_app.jsx
@@ -2,5 +2,5 @@ import '../styles/global.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return <Component {...pageProps} />   
 }
